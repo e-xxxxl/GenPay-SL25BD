197c02543ac0435f40fc786175055b9644b8b975
emmanuel <eajejohnson@gmail.com>
2025-10-25 14:37:24 +0100

    refactor(ping): Cleaned up ping route structure for improved readability

--- routes/pingRoutes.js
+++ routes/pingRoutes.js
@@ -3,13 +3,9 @@ const router = express.Router();
 const pingController = require('../controllers/pingController');
 const rateLimit = require('express-rate-limit');
 
-const pingLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 100, // Allow 100 requests per 10 minutes
-  message: 'Too many ping requests, please try again later.',
-});
 
-router.get('/',pingLimiter, pingController.ping);
+
+router.get('/', pingController.ping);
 
 
 module.exports = router;
